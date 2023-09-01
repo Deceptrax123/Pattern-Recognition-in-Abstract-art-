446ba11adf0343bb13b1ae995dac2e17ee1733fd
Deceptrax123 <srinitishsri@gmail.com>
2023-09-01 16:12:37 +0530

    prep for training

--- training_loops.py
+++ training_loops.py
@@ -1,11 +1,11 @@
 import torch
 from torch.utils.data import DataLoader
 from data_generator import PokemonDataset,AbstractArtDataset
-from discriminator import Discriminator
-from Base_dcgan.generator import Generator
 import numpy as np
 import matplotlib.pyplot as plt
 from initialize import initialize_weights
+from discriminator_modified import Discriminator
+from generator_modified import Generator
 from torch import nn
 from time import time
 import multiprocessing as mp
@@ -13,6 +13,7 @@ import torch.multiprocessing
 import wandb
 from PIL import Image 
 import torchvision.transforms as T
+from torch import mps
 
 def train_step():
     gen_loss=0
@@ -53,21 +54,24 @@ def train_step():
 
         gen_loss+=generator_loss.item()
         dis_loss+=discriminator_loss.item()
+
+        del output_discriminator_generated
+        del real_labels
+        del real_samples
+        del generated_labels
+        del generated_samples
+        del latent_space_samples
+        del out_gen_disc
+        del out_real_disc
+
+        mps.empty_cache()
+
     
     gloss=gen_loss/train_steps
     dloss=dis_loss/train_steps
 
     return gloss,dloss
 
-def log_image():
-    fake=generator(noise)
-
-    fake=fake.to(device='cpu')
-    fake=fake.detach().numpy()
-
-    fake_numpy=fake.transpose(0,3,2,1)
-
-    return fake_numpy
 
 def training_loop():
     discriminator.train(True)
@@ -93,8 +97,8 @@ def training_loop():
 
         #save model at epoch checkpoints
         if(((epoch+1)+250)%25==0):
-            pathgen='./models/abstract_art/resized/generator/generator{number}.pth'.format(number=epoch+1)
-            pathdis='./models/abstract_art/resized/discriminator/discriminator{number}.pth'.format(number=epoch+1)
+            pathgen='./models/abstract_art/modified_arch/generator/generator{number}.pth'.format(number=epoch+1)
+            pathdis='./models/abstract_art/modified_arch/discriminator/discriminator{number}.pth'.format(number=epoch+1)
             torch.save(generator.state_dict(),pathgen)
             torch.save(discriminator.state_dict(),pathdis)
     
@@ -134,20 +138,13 @@ if __name__=='__main__':
     generator=Generator().to(device=device)
     discriminator=Discriminator().to(device=device)
 
-    #initialize_weights(generator)
-    #initialize_weights(discriminator)
+    initialize_weights(generator)
+    initialize_weights(discriminator)
 
-    #start training from checkpoint
-    generator.load_state_dict(torch.load("./models/abstract_art/resized/generator/generator250.pth"))
-    discriminator.load_state_dict(torch.load("./models/abstract_art/resized/discriminator/discriminator250.pth"))
-
-    
-    #testing
-    noise=torch.randn((1,100)).to(device=device)
 
     #hyperparameters
     lr=0.0002
-    num_epochs=750
+    num_epochs=1000
     loss_function=nn.BCEWithLogitsLoss()
 
     #set optimizer
